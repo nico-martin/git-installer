eb5e014354e9e8a3ed5a7d070f07cc2de3028cd2
Nico Martin <mail@nico.dev>
2022-09-23 09:57:37 +0200

    Added dir check for Gilab, closes #5

--- src/Package/provider/Gitlab.php
+++ src/Package/provider/Gitlab.php
@@ -86,6 +86,41 @@ class Gitlab extends Provider
         return $branches;
     }
 
+    private static function getRepoFolderFiles($id, $branch, $folder = '')
+    {
+        $auth = self::authenticateRequest("https://gitlab.com/api/v4/projects/{$id}/repository/tree/?ref={$branch}&recursive=1&per_page=999");
+        $response = Helpers::getRestJson($auth[0], $auth[1]);
+        $files = array_values(
+            array_filter(
+                $response,
+                function ($element) use ($folder) {
+                    if ($element['type'] !== 'blob') return false;
+                    if (!str_starts_with($element['path'], $folder)) return false;
+                    if ($element['path'] === 'style.css') return true;
+                    $relativePath = substr($element['path'], strlen($folder));
+                    if (str_contains($relativePath, '/')) return false;
+                    return str_ends_with($relativePath, '.php');
+                }
+            )
+        );
+
+        return array_map(function ($element) use ($folder, $id, $branch) {
+            $path = urlencode($element['path']);
+            $auth = self::authenticateRequest("https://gitlab.com/api/v4/projects/{$id}/repository/files/{$path}?ref={$branch}");
+            $response = Helpers::getRestJson($auth[0], $auth[1]);
+            return [
+                'file_name' => $response['file_path'],
+                'content' => base64_decode($response['content']),
+            ];
+        }, $files);
+    }
+
+    public static function validateDir($url, $branch, $dir)
+    {
+        $parsed = self::parseGitlabUrl($url);
+        return self::getRepoFolderFiles($parsed['id'], $branch, $dir);
+    }
+
     public static function authenticateRequest($url, $args = [])
     {
         $gitlabToken = sayhelloGitInstaller()->Settings->getSingleSettingValue('git-packages-gitlab-token');
@@ -121,9 +156,9 @@ class Gitlab extends Provider
                 return Gitlab::authenticateRequest($url, $args);
             }
 
-            public function validateDir($url, $dir = '')
+            public function validateDir($url, $branch, $dir = '')
             {
-                return [];
+                return Gitlab::validateDir($url, $branch, $dir);
             }
         };
     }
